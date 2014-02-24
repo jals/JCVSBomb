f5490535635effd56b8a65ca4fdfe01137d3fffb
vbansal1 <vinayak_bansal@carleton.ca>
2014-02-24 12:21:42 -0500

    Send a message from the client to the server.

--- src/bomberman/Client.java
+++ src/bomberman/Client.java
@@ -1,30 +1,48 @@
 package bomberman;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
 	private String ip;
 	private String playerName;
 	private Object[][] grid;
-	
-	public Client(){
-		joinGame();	//Code to make the methods not have warnings
+
+	public Client() throws Exception {
+		joinGame(); // Code to make the methods not have warnings
 		move("up");
 		leaveGame();
 	}
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) throws Exception {
+		new Client();
 	}
-	
-	private void move(String direction){
-		
+
+	private void move(String direction) throws Exception{
+		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IPAddress = InetAddress.getByName(ip);
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+		String sentence = direction;
+		sendData = sentence.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData,
+				sendData.length, IPAddress, 9876);
+		clientSocket.send(sendPacket);
+		DatagramPacket receivePacket = new DatagramPacket(receiveData,
+				receiveData.length);
+		clientSocket.receive(receivePacket);
+		String modifiedSentence = new String(receivePacket.getData());
+		System.out.println("FROM SERVER:" + modifiedSentence);
+		clientSocket.close();
 	}
-	
-	private void joinGame(){
-		
+
+	private void joinGame() {
+
 	}
-	
-	private void leaveGame(){
-		
+
+	private void leaveGame() {
+
 	}
 
 	public String getIp() {
--- src/bomberman/Server.java
+++ src/bomberman/Server.java
@@ -1,18 +1,23 @@
 package bomberman;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
-	
+
 	private List<Player> listOfPlayers;
 	private Object[][] grid;
 
-	public Server(){
+	public Server() {
 		setListOfPlayers(new ArrayList<Player>());
 	}
-	
-	public Server(String filename){
+
+	public Server(String filename) {
 		setListOfPlayers(new ArrayList<Player>());
 	}
 
@@ -31,5 +36,24 @@ public class Server {
 	public void setGrid(Object[][] grid) {
 		this.grid = grid;
 	}
-	
+
+	public static void main (String[] args) throws IOException {
+		 DatagramSocket serverSocket = new DatagramSocket(9876); 
+		 byte[] receiveData = new byte[1024];  
+		 byte[] sendData = new byte[1024];       
+		 while(true)                {         
+			 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);    
+			 serverSocket.receive(receivePacket);   
+			 String sentence = new String( receivePacket.getData());  
+			 System.out.println("RECEIVED: " + sentence);        
+			 InetAddress IPAddress = receivePacket.getAddress();     
+			 int port = receivePacket.getPort();         
+			 String capitalizedSentence = sentence.toUpperCase();    
+			 sendData = capitalizedSentence.getBytes();              
+			 DatagramPacket sendPacket =      
+					 new DatagramPacket(sendData, sendData.length, IPAddress, port);  
+			 serverSocket.send(sendPacket);        
+			 }
+		 }
+
 }
