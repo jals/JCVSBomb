c537037c390d6e758f4fc24fcef400229ff7a524
Sean Byron <sean_byron@carleton.ca>
2014-02-24 12:25:27 -0500

    Refactored Command, added an enum for the different commands. Removed Operation class, now the enum is used

--- src/bomberman/Command.java
+++ src/bomberman/Command.java
@@ -3,7 +3,17 @@ package bomberman;
 import java.io.Serializable;
 
 public class Command implements Serializable{
-
+	
+	public enum Operation {
+		MOVE_LEFT,
+		MOVE_RIGHT,
+		MOVE_UP,
+		MOVE_DOWN, 
+		JOIN_GAME, 
+		LEAVE_GAME,
+		DROP_BOMB,
+	}
+	
 	private static final long serialVersionUID = 7342838579737714576L;
 
 	private String player;
