12ccd3037e9c4eda7898d07f3e093e0c2b87a899
Sean Byron <sean_byron@carleton.ca>
2014-03-03 11:50:43 -0500

    Commented my classes

--- src/bomberman/test/ClientThread.java
+++ src/bomberman/test/ClientThread.java
@@ -2,10 +2,22 @@ package bomberman.test;
 
 import bomberman.Client;
 
+/**
+ * Runs an instance of the client in a separate thread
+ * Used for testing purposes
+ * 
+ * @author spbyron
+ *
+ */
 public class ClientThread extends Thread {
 
+	// Underlying client object, that the thread runs
 	private Client client;
 
+	/**
+	 * Create a new ClientThread, giving the Client the given name
+	 * @param player
+	 */
 	public ClientThread(String player) {
 		try {
 			client = new Client(player, "127.0.0.1", 9876);
@@ -14,11 +26,20 @@ public class ClientThread extends Thread {
 		}
 	}
 
+	/**
+	 * Run the thread
+	 */
 	public void run() {
+		// Run forever
+		// TODO: Stop the thread when the player dies
 		while (true) {
 		}
 	}
 
+	/**
+	 * Returns the Client object that is being run by the thread
+	 * @return
+	 */
 	public Client getClient() {
 		return client;
 	}
--- src/bomberman/test/ServerThread.java
+++ src/bomberman/test/ServerThread.java
@@ -4,11 +4,23 @@ import java.io.IOException;
 
 import bomberman.Server;
 
+/**
+ * Runs an instance of the Server in a separate thread
+ * Used for testing purposes
+ * 
+ * @author spbyron
+ *
+ */
 class ServerThread extends Thread {
 
+	/**
+	 * Run the thread
+	 */
 	public void run() {
 		try {
+			// Start the server in "Test" mode (flag=1) on port 9876
 			String[] args = { "1", "9876" };
+			// Start the server
 			Server.main(args);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -17,6 +29,10 @@ class ServerThread extends Thread {
 		}
 	}
 
+	/**
+	 * Returns the path to the log file the server has been logging into
+	 * @return
+	 */
 	public String getLogFile() {
 		return Server.getLogFile();
 	}
